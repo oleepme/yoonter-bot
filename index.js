8cd656c5cadeac12ba8e72ad79c4783359665b7f
oleepme <gmlsldp1004@naver.com>
2025-12-31 00:51:02 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`봇 로그인됨: ${client.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
